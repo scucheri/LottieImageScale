5ead1da851941bc11f0ec4b721c01c301c74aa2f
xiaoxiaoyu <xiaoxiaoyu@bytedance.com>
2024-08-16 17:13:36 +0800

    图片缩放显示
    Change-Id: I8534ad0b8d5fc9178cf29f0a55c2b6f70eddc577

--- app/src/main/java/com/elyeproj/annotationexperiment/TestLottieView.kt
+++ app/src/main/java/com/elyeproj/annotationexperiment/TestLottieView.kt
@@ -3,16 +3,24 @@ package com.elyeproj.annotationexperiment;
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Matrix
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
 import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.LottieCompositionFactory
 import com.airbnb.lottie.LottieDrawable
-import java.lang.Math.max
+import com.airbnb.lottie.LottieListener
+import com.airbnb.lottie.LottieTask
+
 
 /**
  * Created by xiaoxiaoyu on 2024/8/13.
  */
 class TestLottieView  : LottieAnimationView {
 
+    private var resourceWidth : Float = 1f
+    private var resourceHeight : Float = 1f
     private var index: Int = 0
 
     constructor(context: Context?)  : super(context){
@@ -33,38 +41,60 @@ class TestLottieView  : LottieAnimationView {
         playAnimation()
         repeatMode = LottieDrawable.RESTART
         repeatCount = LottieDrawable.INFINITE
+
+        // 加载动画文件
+        val task = LottieCompositionFactory.fromAsset(context, LOADING_LOTTIE_FILE)
+        task.addListener(object : LottieListener<LottieComposition>{
+            override fun onResult(result: LottieComposition?) {
+                    val result = result ?: return
+                    val bounds = result.getBounds()
+                    resourceWidth = bounds.width().toFloat()
+                    resourceHeight = bounds.height().toFloat()
+                   invalidate()
+            }
+        })
+
+
+        //        setImageDrawable(context.getDrawable(R.drawable.bg_ugc_create_placeholder))
 //        setImageDrawable(context.getDrawable(R.drawable.test_image))
+//image_sketch_loading_3
+//         bitmap = BitmapFactory.decodeResource(resources, R.drawable.bg_ugc_create_placeholder)
+
+
+//        setImageDrawable(context.getDrawable(R.drawable.test_image))
+
+        drawable?.let { // 确保 Drawable 已设置边界
+            drawable.setBounds(0, 0, it.intrinsicWidth, it.intrinsicHeight)
+            resourceWidth =  it.bounds.width().toFloat()
+            resourceHeight = it.bounds.height().toFloat()
+        }
+
     }
 
     override fun onDraw(canvas: Canvas?) {
         canvas ?: return
-        super.onDraw(canvas)
+
         val viewWidth = canvas.width
         val viewHeight = canvas.height
 
-        // 假设动画文件的宽高比为 1:1，则放大并居中显示
-        val scaleFactor = maxOf(viewWidth, viewHeight) / minOf(viewWidth, viewHeight).toFloat()
-        val cutOutX = max(viewHeight - viewWidth, 0).toFloat()
-        val cutOutY = max(viewWidth - viewHeight, 0).toFloat()
-
-        //        val scaleFactor = 2f
         // 创建缩放和平移的Matrix
         val matrix = Matrix().apply {
-            postScale(scaleFactor, scaleFactor)
+            postScale(2* viewWidth.toFloat()/resourceWidth, 2* viewHeight.toFloat()/resourceHeight)
             if (index == 1){
-                postTranslate(-cutOutX, -cutOutY)
+                postTranslate(0f, 0f)
             } else if (index == 2){
-                postTranslate(-viewWidth.toFloat()-cutOutX, 0f-cutOutY)
+                postTranslate(-viewWidth.toFloat(), 0f)
             }else if (index == 3){
-                postTranslate(0f -cutOutX, -viewHeight.toFloat()-cutOutY)
+                postTranslate(0f , -viewHeight.toFloat())
             }else if (index == 4){
-                postTranslate(-viewWidth.toFloat()-cutOutX, -viewHeight.toFloat()-cutOutY)
+                postTranslate(-viewWidth.toFloat(), -viewHeight.toFloat())
             }
         }
         // 应用Matrix到Lottie Animation View
         scaleType = ScaleType.MATRIX
         imageMatrix = matrix
 
+        super.onDraw(canvas)
     }
 
 
